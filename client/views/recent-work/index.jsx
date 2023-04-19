db55e8f1967f3fcd48c5f26aefda8941c1840fb4
Kelby Gassman <kelby.gassman@gmail.com>
2023-04-19 15:50:01 -0400

    Update iFit Nourish url
    Fixed: iFit Nourish discontinued so link resulted in 404

--- client/views/recent-work/index.jsx
+++ client/views/recent-work/index.jsx
@@ -24,8 +24,9 @@ const RecentWork = () => (
       >
         <H3Blurb>Monster</H3Blurb>
         <PBlurb>Connecting people and jobs</PBlurb>
-        <Card height="100px" maxWidth="360px">
+        <Card height="116px" maxWidth="360px">
           <PCard>Role: Sr UX Engineer</PCard>
+          <PCard>Project(s): Multi-tenant design system</PCard>
           <PCard>Platform: React, TypeScript</PCard>
         </Card>
       </Blurb>
@@ -36,19 +37,19 @@ const RecentWork = () => (
       >
         <H3Blurb>iFit</H3Blurb>
         <PBlurb>Health and fitness application</PBlurb>
-        <Card height="100px" maxWidth="360px">
+        <Card height="116px" maxWidth="360px">
           <PCard>Role: Sr Front End Developer</PCard>
           <PCard>Platform: React, Redux</PCard>
         </Card>
       </Blurb>
       <Blurb
-        href="https://shop.ifit.com/pages/nutrition"
+        href="https://www.ifit.com/"
         rel="noopener noreferrer"
         target="_blank"
       >
         <H3Blurb>iFit Nourish</H3Blurb>
         <PBlurb>Custom built nutrition</PBlurb>
-        <Card height="100px" maxWidth="360px">
+        <Card height="116px" maxWidth="360px">
           <PCard>Role: Sr Front End Developer</PCard>
           <PCard>Platform: Shopify</PCard>
         </Card>
@@ -60,7 +61,7 @@ const RecentWork = () => (
       >
         <H3Blurb>LGFIT</H3Blurb>
         <PBlurb>Health and Fitness Coaching</PBlurb>
-        <Card height="100px" maxWidth="360px">
+        <Card height="116px" maxWidth="360px">
           <PCard>Role: Sole Developer</PCard>
           <PCard>Platform: React, Webpack, Netlify</PCard>
         </Card>
@@ -72,7 +73,7 @@ const RecentWork = () => (
       >
         <H3Blurb>Construction Zone</H3Blurb>
         <PBlurb>Construction Truck Memory Game</PBlurb>
-        <Card height="100px" maxWidth="360px">
+        <Card height="116px" maxWidth="360px">
           <PCard>Role: Developer, Artist</PCard>
           <PCard>Platform: GameMaker Studio 2, Aseprite</PCard>
         </Card>
@@ -84,7 +85,7 @@ const RecentWork = () => (
       >
         <H3Blurb>Aquaworld 2</H3Blurb>
         <PBlurb>Game Jam</PBlurb>
-        <Card height="100px" maxWidth="360px">
+        <Card height="116px" maxWidth="360px">
           <PCard>Role: Artist</PCard>
           <PCard>Platform: Unity 3D, Aseprite</PCard>
         </Card>
